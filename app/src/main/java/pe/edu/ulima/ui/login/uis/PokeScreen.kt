98f00577698a3444056d53c5eaa433d66eb965c4
Jonathan Cardenas <greiff.jonathan@gmail.com>
2023-04-23 15:35:38 -0500

    esto

--- app/src/main/java/pe/edu/ulima/ui/login/uis/PokeScreen.kt
+++ app/src/main/java/pe/edu/ulima/ui/login/uis/PokeScreen.kt
@@ -2,6 +2,7 @@ package pe.edu.ulima.ui.login.uis
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,13 +15,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,13 +33,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
 import pe.edu.ulima.R
 import pe.edu.ulima.ui.theme.Gray200
 import pe.edu.ulima.ui.theme.Orange200
@@ -48,15 +58,17 @@ public fun PokeScreenPreview(){
 @Composable
 public fun PokeScreen(){
 
-    var numSeguidores = 52
-    var numSeguidos = 55
-    var numPub = 52
+    //variables de modificacion
+
+    val nombreUsuario = "Carlos Torres"
+    val numSeguidores = 52
+    val numSeguidos = 55
+    val numPub = 52
 
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(all = 10.dp),
-        contentAlignment = Alignment.Center
+            .padding(all = 15.dp),
     ){
 
         Column(
@@ -64,7 +76,7 @@ public fun PokeScreen(){
             horizontalAlignment = Alignment.CenterHorizontally) {
 
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
@@ -72,18 +84,19 @@ public fun PokeScreen(){
                     horizontalAlignment = Alignment.CenterHorizontally
                 ){
                     Image(
-                        painter = painterResource(id = R.drawable.ic_ulima),
+                        painter = rememberImagePainter(data = "https://i0.wp.com/lamiradafotografia.es/wp-content/uploads/2014/07/foto-perfil-psicologo-180x180.jpg?resize=180%2C180"),
                         contentDescription = "Foto Perfil",
+                        contentScale = ContentScale.Crop,
+                        alignment = Alignment.CenterEnd,
                         modifier = Modifier
-                            .size(120.dp)
-                            .padding(bottom = 10.dp),
-                        colorFilter = ColorFilter.tint(
-                            color = if(isSystemInDarkTheme()) Orange200 else Gray200
-                        )
+                            .size(60.dp)
+                            .clip(CircleShape)
                     )
                     Text(
-                        text = "Carlos Torres",
+                        text = nombreUsuario,
                         textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(top = 10.dp)
                     )
                 }
                 Column(
@@ -92,6 +105,8 @@ public fun PokeScreen(){
                     Text(
                         text = Integer.toString(numPub),
                         textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp
                     )
                     Text(
                         text = "Publicaciones",
@@ -104,6 +119,8 @@ public fun PokeScreen(){
                     Text(
                         text = Integer.toString(numSeguidos),
                         textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp
                     )
                     Text(
                         text = "Seguidos",
@@ -116,6 +133,8 @@ public fun PokeScreen(){
                     Text(
                         text = Integer.toString(numSeguidores),
                         textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp
                     )
                     Text(
                         text = "Seguidores",
@@ -125,54 +144,59 @@ public fun PokeScreen(){
             }
 
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
 
                 Button(
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Orange200),
                     onClick = {
                         println("Presiono editar perfil")
                     }
                 ){
-                    Text("Editar Perfil")
+                    Text("Editar Perfil",
+                    color =  if(isSystemInDarkTheme()) Color.White else Color.Black)
                 }
                 Button(
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Orange200),
                     onClick = {
                         println("Presiono compartir perfil")
                     }
                 ){
-                    Text("Compartir Perfil")
+                    Text("Compartir Perfil",
+                        color =  if(isSystemInDarkTheme()) Color.White else Color.Black)
                 }
                 Button(
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Orange200),
                     onClick = {
                         println("Presiono agregar algo")
                     }
                 ){
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_ulima),
-                        contentDescription = "Foto Perfil",
-                        modifier = Modifier
-                            .size(15.dp),
-                        colorFilter = ColorFilter.tint(
-                            color = if(isSystemInDarkTheme()) Orange200 else Gray200
-                        )
-                    )
-
+                    Icon(Icons.Default.Person,
+                        contentDescription = "content description",
+                        tint =  if(isSystemInDarkTheme()) Color.White else Color.Black)
+                    Icon(Icons.Default.Add,
+                        contentDescription = "content description",
+                        tint =  if(isSystemInDarkTheme()) Color.White else Color.Black)
                 }
             }
 
             Column(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp),
                 horizontalAlignment = Alignment.Start
             ){
                 Text(
                     text = "Historias destacadas",
                     textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 5.dp)
                 )
                 Text(
                     text = "Guarda tus historias favoritas en el perfil",
                     textAlign = TextAlign.Center,
+                    fontSize = 12.sp,
+                    modifier = Modifier.padding(bottom = 10.dp)
                 )
 
                 Column(
@@ -181,22 +205,30 @@ public fun PokeScreen(){
                     OutlinedButton(
                         modifier= Modifier.size(50.dp),  //avoid the oval shape
                         shape = CircleShape,
-                        border= BorderStroke(3.dp, Color.Black),
+                        border= BorderStroke(3.dp, if(isSystemInDarkTheme()) Color.White else Color.Black),
                         contentPadding = PaddingValues(0.dp),
                         onClick = {
                             println("Crea nueva historia")
                         }
                     ){
-                        Icon(Icons.Default.Add, contentDescription = "content description")
+                        Icon(
+                            Icons.Default.Add,
+                            modifier= Modifier.size(45.dp),
+                            contentDescription = "content description",
+                            tint =  if(isSystemInDarkTheme()) Color.White else Color.Black)
                     }
 
                     Text(
                         text = "Nueva",
                         textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(top = 5.dp),
+                        fontSize = 12.sp
                     )
                 }
 
             }
+
+            //Esta es la parte para el array
 
             Row(
 
